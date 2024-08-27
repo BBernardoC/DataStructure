58e0f7d999a940f3b578120f0f22b61095016b2d
Bernardo ;) <108904877+BBernardoC@users.noreply.github.com>
2024-08-26 21:31:50 -0300

    Update CircularQueue.java

--- Queue/CircularQueue.java
+++ Queue/CircularQueue.java
@@ -1,63 +1,56 @@
 public class CircularQueue<T> {
-    private T[] queue;
-    private int front, rear, size;
-    private int capacity;
+    private int primeiro;
+    private int ultimo;
+    private T[] dados;
+    private int capacidade;
+    private int tamanho;
 
-    @SuppressWarnings("unchecked")
-    public CircularQueue(int capacity) {
-        this.capacity = capacity;
-        this.queue = (T[]) new Object[capacity];
-        this.front = 0;
-        this.rear = -1;
-        this.size = 0;
+    public CircularQueue(int capacidade) {
+        this.capacidade = capacidade;
+        dados = (T[]) new Object[this.capacidade]; // Inicialização do array genérico
+        primeiro = 0;
+        ultimo = -1;
+        tamanho = 0;
     }
 
-    // Enqueue operation
-    public void enqueue(T item) throws IllegalStateException {
-        if (isFull()) {
-            throw new IllegalStateException("Queue is full");
+    public boolean vazia() {
+        return tamanho == 0;
+    }
+    
+    public boolean cheia() {
+        return tamanho == capacidade;
+    }
+
+    public void imprime() {
+        if (vazia()) {
+            System.out.println("Fila vazia");
+        } else {
+            for (int i = 0; i < tamanho; i++) {
+                int index = (primeiro + i) % capacidade;
+                System.out.println(dados[index]);
+            }
         }
-        rear = (rear + 1) % capacity;
-        queue[rear] = item;
-        size++;
     }
 
-    // Dequeue operation
-    public T dequeue() throws NoSuchElementException {
-        if (isEmpty()) {
-            throw new NoSuchElementException("Queue is empty");
+    public void insere(T valor) {
+        if (cheia()) {
+            System.out.println("Fila cheia");
+        } else {
+            ultimo = (ultimo + 1) % capacidade;
+            dados[ultimo] = valor;
+            tamanho++;
         }
-        T item = queue[front];
-        queue[front] = null; // Optional: Clear the reference to help GC
-        front = (front + 1) % capacity;
-        size--;
-        return item;
     }
-
-    // Peek the front element
-    public T peek() throws NoSuchElementException {
-        if (isEmpty()) {
-            throw new NoSuchElementException("Queue is empty");
+    
+    public T remove() {
+        if (vazia()) {
+            System.out.println("Fila vazia");
+            return null;
+        } else {
+            T valor = dados[primeiro];
+            primeiro = (primeiro + 1) % capacidade;
+            tamanho--;
+            return valor;
         }
-        return queue[front];
     }
-
-    // Check if the queue is empty
-    public boolean isEmpty() {
-        return size == 0;
-    }
-
-    // Check if the queue is full
-    public boolean isFull() {
-        return size == capacity;
-    }
-
-    // Get the current size of the queue
-    public int size() {
-        return size;
-    }
-
-    // Get the capacity of the queue
-    public int capacity() {
-        return capacity;
-    }
+}
